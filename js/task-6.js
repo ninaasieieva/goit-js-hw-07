594fc8e6f4eaaafc31b14457dcdf3b1b33d0ca73
ninaasieieva <160421199+ninaasieieva@users.noreply.github.com>
2024-06-09 21:27:04 +0300

    5

--- js/task-6.js
+++ js/task-6.js
@@ -3,43 +3,35 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
-/* ---------------------------------- */
+const controls = document.querySelector("#controls");
+const input = controls.querySelector('input[type="number"]');
+const createBtn = controls.querySelector("button[data-create]");
+const destroyBtn = controls.querySelector("button[data-destroy]");
+const boxes = document.getElementById("boxes");
 
-const controls = document.getElementById("controls");
-const numberInput = controls.querySelector('input[type="number"]');
-const createButton = controls.querySelector("[data-create]");
-const destroyButton = controls.querySelector("[data-destroy]");
-const boxesDiv = document.getElementById("boxes");
-
-createButton.addEventListener("click", () => {
-  const qty = parseInt(numberInput.value);
-  if (qty >= 1 && qty <= 100) {
-    createBoxes(qty);
-    numberInput.value = "";
-  } else {
-    alert("Please enter a number between 1 and 100");
+createBtn.addEventListener("click", () => {
+  const amount = parseInt(input.value);
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    input.value = "";
   }
 });
 
-destroyButton.addEventListener("click", destroyBoxes);
+destroyBtn.addEventListener("click", destroyBoxes);
 
-function destroyBoxes() {
-  boxesDiv.innerHTML = "";
+function createBoxes(amount) {
+  destroyBoxes();
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < amount; i++) {
+    const div = document.createElement("div");
+    div.style.width = `${30 + i * 10}px`;
+    div.style.height = `${30 + i * 10}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    fragment.appendChild(div);
+  }
+  boxes.appendChild(fragment);
 }
 
-function createBoxes(qty) {
-  destroyBoxes();
-  const elements = [];
-  let size = 30;
-  for ( size ; size < (30 + qty * 10); size += 10) {
-    const box = document.createElement("div");
-    box.style.width = `${size}px`;
-    box.style.height = `${size}px`;
-    box.style.backgroundColor = getRandomHexColor();
-    elements.push(box); 
-  }
-  if (size > 426) {
-    boxesDiv.style.width = size + 64 +"px";
-  } /* changes width of Div if size of Box larger than Div (+ padding32 *2 )*/
-  boxesDiv.append(...elements);
+function destroyBoxes() {
+  boxes.innerHTML = "";
 }
